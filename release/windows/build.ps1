884803137af4ef199af2fac6b8ac4671f36b4c66
Alex <xitonix.io@gmail.com>
2020-09-13 09:45:14 +1000

    Removed unused wix variables

--- release/windows/build.ps1
+++ release/windows/build.ps1
@@ -3,4 +3,4 @@ param($out,$version='')
 $buildTime = (Get-Date).ToUniversalTime() | Get-Date -UFormat '%Y-%m-%dT%TZ'
 $env:GOOS = "windows"
 $env:GOARCH = "amd64"
-go build -o $out -ldflags="-X main.version=v$version -X main.commit=$env:GITHUB_SHA -X main.built=$buildTime" *.go
+go build -o $out -ldflags="-X main.version=v$version -X main.commit=$env:GITHUB_SHA -X main.built=$buildTime" "*.go"
