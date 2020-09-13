6476eff22950b7f3a3a4e14cc9f0a7a2d7f109bb
Alex <xitonix.io@gmail.com>
2020-09-13 10:31:32 +1000

    Fixed build.ps1

--- release/windows/build.ps1
+++ release/windows/build.ps1
@@ -1,6 +1,6 @@
-param($binary,$version='')
+param($binary,$version)
 
 $buildTime = (Get-Date).ToUniversalTime() | Get-Date -UFormat '%Y-%m-%dT%TZ'
 $env:GOOS = "windows"
 $env:GOARCH = "amd64"
-go build -o $out.exe -ldflags="-s -w -X main.version=v$version -X main.commit=$env:GITHUB_SHA -X 'main.built=$buildTime'" github.com/xitonix/$binary
+go build -o "$binary.exe" -ldflags="-s -w -X main.version=v$version -X main.commit=$env:GITHUB_SHA -X 'main.built=$buildTime'"
