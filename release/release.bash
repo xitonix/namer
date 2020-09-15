d146958f3a463290a10bbacf80ddf86155683152
Alex <xitonix.io@gmail.com>
2020-09-16 09:20:31 +1000

    more logging

--- release/release.bash
+++ release/release.bash
@@ -11,6 +11,11 @@ mkdir -p $BIN_DIR
 echo "Creating ${RELEASE_NAME}.tar.gz..." 1>&2
 "$SRC/release/build.bash" "$BIN_DIR/$BINARY" "$RELEASE_VERSION"
 tar -C "${BIN_DIR}" -cvzf "${RELEASE_NAME}.tar.gz" "${BINARY}"
+docker version
+ls
+pwd
+ls ${BIN_DIR}
+ls ${SRC}/bin
 docker run --rm -it -v $(pwd):/app xitonix/fpm-rpm -s tar -t rpm -n ${BINARY} -p /app -v ${RELEASE_VERSION} --iteration ${RPM_ITERATION} "/app/${RELEASE_NAME}.tar.gz"
 echo "::set-output name=file::${RELEASE_NAME}.tar.gz"
 echo "::set-output name=rpm::${BINARY}-${RELEASE_VERSION}-${RPM_ITERATION}.x86_64.rpm
