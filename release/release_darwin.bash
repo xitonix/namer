e0a9913abe9d006fd98de2ec64be29fbeded926c
Alex <xitonix.io@gmail.com>
2020-09-17 17:25:39 +1000

    Fixed bash

--- release/release_darwin.bash
+++ release/release_darwin.bash
@@ -13,4 +13,4 @@ echo "Creating ${RELEASE_NAME}.tar.gz..." 1>&2
 tar -C "${BIN_DIR}" -cvzf "${RELEASE_NAME}.tar.gz" "${BINARY}"
 echo "::set-output name=file::${RELEASE_NAME}.tar.gz"
 echo "::set-output name=sha::$(shasum -a 256 ${RELEASE_NAME}.tar.gz | awk '{printf $1}')"
-echo "::set-output name=version::"v${RELEASE_VERSION}"
+echo "::set-output name=version::v${RELEASE_VERSION}"
